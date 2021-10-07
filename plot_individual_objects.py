3c1b5b608073527cc055e97bfac9db560860e6e3
Masaya Yamamoto <my137@duke.edu>
2021-10-07 15:38:06 -0400

    plot individual objects in a coadd slice

--- plot_individual_objects.py
+++ plot_individual_objects.py
@@ -3,6 +3,7 @@ import fitsio as fio
 import numpy as np
 import os, sys
 import meds
+from matplotlib import pyplot as plt
 
 work = '/data/des70.a/data/masaya/'
 tilenames = ['DES2208-5123', 'DES0234-0207', 'DES0326-2041']
@@ -13,6 +14,7 @@ coadd_fnames = fio.read(os.path.join('/data/des70.a/data/masaya/pizza-slice/v2',
 # r_epochs = coadd['epochs_info'].read()
 # r_image_info = coadd['image_info'].read()
 
+outliers = {}
 for t in tilenames:
     mdet = mdet_obj[mdet_obj['TILENAME']==t]
     coadd_f = coadd_fnames[((coadd_fnames['FILENAME'][:12]==t) & (coadd_fnames['BAND']=='r'))]['FILENAME']
@@ -20,5 +22,20 @@ for t in tilenames:
     for obj in mdet:
         if obj['MDET_T'] > 1.8:
             slice_id = obj['SLICE_ID']
-            coadd_image = m.get_cutout(slice_id, 0)
-            fio.write('/data/des70.a/data/masaya/pizza-slice/coadd_image/'+str(slice_id)+'_coadd.fits', coadd_image)
+            if slice_id not in list(outliers.keys()):
+                outliers[slice_id] = [(obj['SLICE_ROW'], obj['SLICE_COL'])]
+            else:
+                outliers[slice_id].append((obj['SLICE_ROW'], obj['SLICE_COL']))
+
+    for s in list(outliers.keys()):
+        coadd_image = m.get_cutout(s, 0)
+    
+        fig, ax = plt.subplots(10,10)
+        ax.imshow(coadd_image)
+        plt.colorbar()
+        for i in range(len(outliers[s])):
+            ax.scatter(outliers[s][0], outliers[s][1], color='r')
+        plt.savefig('/data/des70.a/data/masaya/pizza-slice/coadd_image/'+str(slice_id)+'_coadd.png')
+        plt.clf()
+            
+# fio.write('/data/des70.a/data/masaya/pizza-slice/coadd_image/'+str(slice_id)+'_coadd.fits', coadd_image)
