e75677be1ba903acece607803e489e20be3d589e
Masaya Yamamoto <my137@duke.edu>
2021-10-08 10:54:40 -0400

    bug

--- plot_individual_objects.py
+++ plot_individual_objects.py
@@ -4,6 +4,7 @@ import numpy as np
 import os, sys
 import meds
 from matplotlib import pyplot as plt
+from tqdm import tqdm
 
 work = '/data/des70.a/data/masaya/'
 tilenames = ['DES2208-5123', 'DES0234-0207', 'DES0326-2041']
@@ -21,14 +22,14 @@ for t in tilenames:
     mask = ((tilenames==t) & (coadd_fnames['BAND']=='r'))
     coadd_f = coadd_fnames[mask]['FILENAME'][0]
     m = meds.MEDS(os.path.join('/data/des70.a/data/masaya/pizza-slice/v2/r_band/', coadd_f))
-    for obj in mdet:
+    for obj in tqdm(mdet):
         if obj['MDET_T'] > 1.8:
             slice_id = obj['SLICE_ID']
             if slice_id not in list(outliers.keys()):
                 outliers[slice_id] = [(obj['SLICE_ROW'], obj['SLICE_COL'])]
             else:
                 outliers[slice_id].append((obj['SLICE_ROW'], obj['SLICE_COL']))
-
+    print(outliers)
     for s in list(outliers.keys()):
         coadd_image = m.get_cutout(s, 0)
     
