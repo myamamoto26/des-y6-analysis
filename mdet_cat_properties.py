f0411c2e98084097aa474a0eb8e01735a2aa67f0
Masaya Yamamoto <my137@duke.edu>
2021-10-08 14:20:26 -0400

    tratio

--- mdet_cat_properties.py
+++ mdet_cat_properties.py
@@ -182,7 +182,7 @@ def mdet_shear_pairs_plotting_percentile(d, nperbin, cut_quantity):
         g_obs = np.zeros(bin_num)
         gerr_obs = np.zeros(bin_num)
         for i in tqdm(range(bin_num)):
-            additional_cuts = {'quantity': cut_quantity, 'cuts': [hist['low'][i], hist['high'][i]]}
+            additional_cuts = {'quantity': 'MDET_T_RATIO', 'cuts': [hist['low'][i], hist['high'][i]]}
             R, g_mean, gerr_mean, bs = calculate_response(d, additional_cuts=additional_cuts)
             g_obs[i] = g_mean[q%2]/R[q%2]
             gerr_obs[i] = (gerr_mean[q%2]/R[q%2])
