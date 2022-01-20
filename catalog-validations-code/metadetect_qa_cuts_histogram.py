8ae143c43c2cb1e63f2566d0ad055fbc7bf798c9
Masaya Yamamoto <my137@duke.edu>
2022-01-20 11:22:04 -0500

    histogram

--- catalog-validations-code/metadetect_qa_cuts_histogram.py
+++ catalog-validations-code/metadetect_qa_cuts_histogram.py
@@ -11,10 +11,12 @@ def _get_hist_with_cuts(d, qa_cut_quantity, qa_cut):
     
     fig,ax=plt.subplots(1,3,figsize=(15,7))
     label = 'QA Cuts: '
+    total = len(d)
     for i in range(len(qa_cut_quantity)):
         msk = (d[qa_cut_quantity[i]] < qa_cut[i])
         d = d[msk]
         label += qa_cut_quantity[i]+'<'+str(qa_cut[i])+' '
+    label2 = 'Remaining: '+str(total*100/len(d))
     
     ax[0].hist(d['MDET_S2N'], bins=100000, histtype='step')
     ax[0].set_xlabel('S/N', fontsize=20)
@@ -28,6 +30,6 @@ def _get_hist_with_cuts(d, qa_cut_quantity, qa_cut):
     ax[2].set_xlabel(r'$T_{ratio}$', fontsize=20)
     ax[2].tick_params(labelsize=15)
     
-    plt.title(label, fontsize=15)
+    ax[1].title(label, fontsize=15)
     plt.tight_layout()
     plt.savefig('mdet_qa_cuts.pdf', bbox_inches='tight')
