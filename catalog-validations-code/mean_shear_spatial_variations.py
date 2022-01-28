e62eb7dc4f5b50ac964f17a5c2830c7fccf47801
Masaya Yamamoto <my137@duke.edu>
2022-01-28 15:02:11 -0500

    new cut

--- catalog-validations-code/mean_shear_spatial_variations.py
+++ catalog-validations-code/mean_shear_spatial_variations.py
@@ -407,7 +407,7 @@ def main(argv):
         t = tilenames[sys.argv[1]]
         ccdres = {}
         d = fio.read(os.path.join(work_mdet, mdet_filenames[np.where(tilenames == t)[0][0]]))
-        msk = ((d['flags']==0) & (d['mask_flags']==0) & (d['mdet_s2n']>10) & (d['mdet_s2n']<100) & (d['mfrac']<0.02) & (d['mdet_T_ratio']>0.5) & (d['mdet_T']<2.5))
+        msk = ((d['flags']==0) & (d['mask_flags']==0) & (d['mdet_s2n']>10) & (d['mdet_s2n']<100) & (d['mfrac']<0.02) & (d['mdet_T_ratio']>0.5) & (d['mdet_T']<1.2))
         ccdres = spatial_variations(ccdres, d[msk], coadd_files[t], ccd_x_min, ccd_y_min, x_side, y_side, piece_side, t, bands[t])
         with open('/global/cscratch1/sd/myamamot/metadetect/mdet_shear_focal_plane_'+t+'.pickle', 'wb') as raw:
             pickle.dump(ccdres, raw, protocol=pickle.HIGHEST_PROTOCOL)
