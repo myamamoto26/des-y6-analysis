fa3770a7f7f9244dbcdbf75145f0a060dfb951bc
Masaya Yamamoto <my137@duke.edu>
2022-01-28 14:18:41 -0500

    new cat

--- catalog-validations-code/metadetect_qa_cuts_histogram.py
+++ catalog-validations-code/metadetect_qa_cuts_histogram.py
@@ -38,9 +38,13 @@ def _get_hist_with_upper_cuts(d, qa_upper_cuts, qa_upper_cuts_value):
     plt.tight_layout()
     plt.savefig('mdet_qa_v1_cuts.pdf', bbox_inches='tight')
 
-def _predefine_bins(d, binname, fname, qa_upper_cuts, qa_upper_cuts_value, nperbin):
+def _predefine_bins(d, binname, fname, qa_lower_cuts, qa_lower_cuts_value, qa_upper_cuts, qa_upper_cuts_value, nperbin):
     
-    # apply cuts
+    # apply lower cuts
+    for i in range(len(qa_lower_cuts)):
+        msk = (d[qa_lower_cuts[i]] > qa_lower_cuts_value[i])
+        d = d[msk]
+    # apply upper cuts
     for i in range(len(qa_upper_cuts)):
         msk = (d[qa_upper_cuts[i]] < qa_upper_cuts_value[i])
         d = d[msk]
