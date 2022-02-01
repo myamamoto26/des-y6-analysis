872f6b500f93e6ba8b152a5196b30d85b28b97b1
Masaya Yamamoto <my137@duke.edu>
2022-02-01 15:33:37 -0500

    bug

--- catalog-validations-code/mean_shear_spatial_variations.py
+++ catalog-validations-code/mean_shear_spatial_variations.py
@@ -409,6 +409,7 @@ def main(argv):
         split_tilenames = np.array_split(tilenames, size)
         for t in split_tilenames[rank]:
             ccdres = {}
+            print(t, np.where(tilenames == t)[0])
             d = fio.read(os.path.join(work_mdet, mdet_filenames[np.where(tilenames == t)[0][0]]))
             msk = ((d['flags']==0) & (d['mask_flags']==0) & (d['mdet_s2n']>10) & (d['mdet_s2n']<100) & (d['mfrac']<0.02) & (d['mdet_T_ratio']>0.5) & (d['mdet_T']<1.2))
             ccdres = spatial_variations(ccdres, d[msk], coadd_files[t], ccd_x_min, ccd_y_min, x_side, y_side, piece_side, t, bands[t])
