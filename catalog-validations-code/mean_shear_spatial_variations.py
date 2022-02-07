9e9acb236c444fc41c3a2aa81d623a86749587db
Masaya Yamamoto <my137@duke.edu>
2022-02-07 11:19:54 -0500

    bug

--- catalog-validations-code/mean_shear_spatial_variations.py
+++ catalog-validations-code/mean_shear_spatial_variations.py
@@ -160,6 +160,7 @@ def spatial_variations(ccdres, mdet_obj, coadd_files, ccd_x_min, ccd_y_min, x_si
             if ccdnum not in list(ccdres):
                 ccdres[ccdnum] = {}
             mdet_step = mdet_obj["mdet_step"][msk_obj]
+            print(len(mdet_obj["mdet_g_1"][msk_obj]))
             ccdres = _accum_shear(ccdres, ccdnum, "g1", "noshear", mdet_step, xind, yind, mdet_obj["mdet_g_1"][msk_obj], x_side, y_side)
             ccdres = _accum_shear(ccdres, ccdnum, "g2", "noshear", mdet_step, xind, yind, mdet_obj["mdet_g_2"][msk_obj], x_side, y_side)
             ccdres = _accum_shear(ccdres, ccdnum, "g1p", "1p", mdet_step, xind, yind, mdet_obj["mdet_g_1"][msk_obj], x_side, y_side)
@@ -414,8 +415,9 @@ def main(argv):
             if not os.path.exists('/global/cscratch1/sd/myamamot/metadetect/mdet_shear_focal_plane_'+t+'.pickle'):
                 d = fio.read(os.path.join(work_mdet, mdet_filenames[np.where(np.in1d(tilenames, t))[0][0]]))
                 msk = ((d['flags']==0) & (d['mask_flags']==0) & (d['mdet_s2n']>10) & (d['mdet_s2n']<100) & (d['mfrac']<0.02) & (d['mdet_T_ratio']>0.5) & (d['mdet_T']<1.2))
-                print(d[msk], coadd_files[t], bands[t])
+                print(len(d[msk]), coadd_files[t], bands[t])
                 ccdres = spatial_variations(ccdres, d[msk], coadd_files[t], ccd_x_min, ccd_y_min, x_side, y_side, piece_side, t, bands[t])
+                print(ccdres)
                 with open('/global/cscratch1/sd/myamamot/metadetect/mdet_shear_focal_plane_'+t+'.pickle', 'wb') as raw:
                     pickle.dump(ccdres, raw, protocol=pickle.HIGHEST_PROTOCOL)
             else:
