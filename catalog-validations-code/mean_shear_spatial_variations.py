778ea7c75b60a708f2aeabf6905e66c2414a1c20
Masaya Yamamoto <my137@duke.edu>
2022-02-17 14:57:13 -0500

    trim edges

--- catalog-validations-code/mean_shear_spatial_variations.py
+++ catalog-validations-code/mean_shear_spatial_variations.py
@@ -273,7 +273,7 @@ def plot_stacked_xy(x_side, y_side, ccdres, xbin, ybin, plot=False, jc=None):
         cmap.set_bad(color='k', alpha=1.)
         piece_side = 32
         print(mean_g1.shape)
-        X, Y = np.meshgrid(np.linspace(1, 3873, (3872//piece_side)+1), np.linspace(1, 1825, (1824//piece_side)+1))
+        X, Y = np.meshgrid(np.linspace(1, 3873, (3872//piece_side)+2), np.linspace(1, 1825, (1824//piece_side)+2))
 
         mean = np.nanmean(mean_g1[mean_g1 > -10])
         sig = np.nanstd(mean_g1[mean_g1 > -10]) / np.sqrt(mean_g1[mean_g1 > -10].size)
