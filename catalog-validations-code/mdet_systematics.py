71ac2dde8cbc8afdfb004da144868f2a85fabebc
Masaya Yamamoto <my137@duke.edu>
2022-02-23 21:48:13 -0500

    update cuts

--- catalog-validations-code/mdet_systematics.py
+++ catalog-validations-code/mdet_systematics.py
@@ -506,7 +506,7 @@ def tangential_shear_field_center():
         
     # Compute the shear response over all the tiles. 
     save_objects = True
-    f = open('/global/cscratch1/sd/myamamot/metadetect/mdet_files.txt', 'r')
+    f = open('/global/project/projectdirs/des/myamamot/mdet_files.txt', 'r')
     fs = f.read().split('\n')[:-1]
     mdet_filenames = [fname.split('/')[-1] for fname in fs]
     tilenames = [d.split('_')[0] for d in mdet_filenames] 
@@ -523,8 +523,17 @@ def tangential_shear_field_center():
     if save_objects:
         # For each tilename, save a file that contains each object's location, shear, and field centers. 
         for t in tqdm(tilenames):
-            d = fio.read(os.path.join('/global/cscratch1/sd/myamamot/metadetect', mdet_filenames[np.where(np.in1d(tilenames, t))[0][0]]))
-            msk = ((d['flags']==0) & (d['mask_flags']==0) & (d['mdet_s2n']>10) & (d['mdet_s2n']<100) & (d['mfrac']<0.02) & (d['mdet_T_ratio']>0.5) & (d['mdet_T'] <1.2))
+            d = fio.read(os.path.join('/global/project/projectdirs/des/myamamot/metadetect', mdet_filenames[np.where(np.in1d(tilenames, t))[0][0]]))
+            mag_g = 30.0 - 2.5*np.log10(d["mdet_g_flux"])
+            mag_r = 30.0 - 2.5*np.log10(d["mdet_r_flux"])
+            mag_i = 30.0 - 2.5*np.log10(d["mdet_i_flux"])
+            mag_z = 30.0 - 2.5*np.log10(d["mdet_z_flux"])
+            gmr = mag_g - mag_r
+            rmi = mag_r - mag_i
+            imz = mag_i - mag_z
+
+            msk = ((d["flags"] == 0) & (d["mask_flags"] == 0) & (d["mdet_flux_flags"] == 0) & (d["mdet_T_ratio"] > 0.5) & (d["mdet_s2n"] > 10) & (d["mfrac"] < 0.1) & (d["mdet_T"] < 1.9 - 2.8*d["mdet_T_err"]) & (np.abs(gmr) < 5) & (np.abs(rmi) < 5) & (np.abs(imz) < 5) & np.isfinite(mag_g) & np.isfinite(mag_r) & np.isfinite(mag_i) & np.isfinite(mag_z) & (mag_g < 26.5) & (mag_r < 26.5) & (mag_i < 26.2) & (mag_z < 25.6))
+            # msk = ((d['flags']==0) & (d['mask_flags']==0) & (d['mdet_s2n']>10) & (d['mdet_s2n']<100) & (d['mfrac']<0.02) & (d['mdet_T_ratio']>0.5) & (d['mdet_T'] <1.2))
             find_and_save_objects(t, d[msk], R11, R22, expnum_field_centers)
     else:
         bin_config = dict(
