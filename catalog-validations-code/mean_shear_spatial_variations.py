f4fcf9b0cd64cbc7fd8184fb1e346ce73542d83c
Masaya Yamamoto <my137@duke.edu>
2022-03-02 14:55:30 -0500

    make per ccd and all files

--- catalog-validations-code/mean_shear_spatial_variations.py
+++ catalog-validations-code/mean_shear_spatial_variations.py
@@ -54,11 +54,8 @@ def _accum_shear_per_ccd(ccdres_all, ccdres, tilename):
     cnames = ['g1', 'g2', 'g1p', 'g1m', 'g2p', 'g2m']
     for ccdnum in list(ccdres):
         for cname in cnames:
-            rows,cols = np.where(~np.isnan(ccdres[ccdnum][cname]))
-            np.add.at(ccdres_all[ccdnum][tilename][cname], (rows, cols), ccdres[ccdnum][cname][rows, cols])
-            np.add.at(ccdres_all[ccdnum][tilename]["num_"+cname], (rows, cols), ccdres[ccdnum]["num_"+cname][rows, cols])
-            # ccdres_all[ccdnum][tilename][cname] = ccdres[ccdnum][cname]
-            # ccdres_all[ccdnum][tilename]["num_"+cname] = ccdres[ccdnum]["num_"+cname]
+            ccdres_all[ccdnum][tilename][cname] = ccdres[ccdnum][cname]
+            ccdres_all[ccdnum][tilename]["num_"+cname] = ccdres[ccdnum]["num_"+cname]
 
     return ccdres_all
 
