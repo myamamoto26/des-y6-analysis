86de65a9973ed74be8a9d28e33ab2124caac0396
Masaya Yamamoto <my137@duke.edu>
2022-03-18 14:55:39 -0400

    methodolgoy fixed

--- catalog-validations-code/mdet_systematics.py
+++ catalog-validations-code/mdet_systematics.py
@@ -387,7 +387,7 @@ def tangential_shear_field_center(fs):
         
     # Compute the shear response over all the tiles. 
     save_objects = False
-    random_only = True
+    random_only = False
     mdet_filenames = [fname.split('/')[-1] for fname in fs]
     tilenames = [d.split('_')[0] for d in mdet_filenames]
     if not os.path.exists('/global/cscratch1/sd/myamamot/metadetect/shear_response_v2.txt'):
@@ -425,12 +425,22 @@ def tangential_shear_field_center(fs):
         if not random_only:
             cat1_file = '/global/cscratch1/sd/myamamot/pizza-slice/exposure_field_centers.fits'
             cat1 = treecorr.Catalog(cat1_file, ra_col='AVG(I.RA_CENT)', dec_col='AVG(I.DEC_CENT)', ra_units='deg', dec_units='deg', npatch=20)
-            cat2_files = glob.glob('/global/cscratch1/sd/myamamot/metadetect/field_centers/mdet_shear_field_centers_*.fits')
-            cat2_list = [treecorr.Catalog(cat2_file, ra_col='ra_obj', dec_col='dec_obj', ra_units='deg', dec_units='deg', g1_col='g1', g2_col='g2', patch_centers=cat1.patch_centers) for cat2_file in cat2_files]
-
+            # cat2_files = glob.glob('/global/cscratch1/sd/myamamot/metadetect/field_centers/mdet_shear_field_centers_*.fits')
+            # cat2_list = [treecorr.Catalog(cat2_file, ra_col='ra_obj', dec_col='dec_obj', ra_units='deg', dec_units='deg', g1_col='g1', g2_col='g2', patch_centers=cat1.patch_centers) for cat2_file in cat2_files]
+            # ng = treecorr.NGCorrelation(bin_config, verbose=2)
+            # for i,cat2 in tqdm(enumerate(cat2_list)):
+            #     ng.process(cat1, cat2, initialize=(i==0), finalize=(i==len(cat2_list)-1))
+            #     cat2.unload()
+            cat2_files = glob.glob('/global/project/projectdirs/des/myamamot/metadetect/cuts_v2/*_metadetect-v5_mdetcat_part0000.fits')
             ng = treecorr.NGCorrelation(bin_config, verbose=2)
-            for i,cat2 in tqdm(enumerate(cat2_list)):
-                ng.process(cat1, cat2, initialize=(i==0), finalize=(i==len(cat2_list)-1))
+            for i,cat2_f in enumerate(cat2_files):
+                d = fio.read(cat2_f)
+                mask_noshear = (d['mdet_step'] == 'noshear')
+                g1 = d[mask_noshear]['mdet_g_1']/np.float64(R11)
+                g2 = d[mask_noshear]['mdet_g_2']/np.float64(R22)
+                cat2 = treecorr.Catalog(ra=d[mask_noshear]['ra'], dec=d[mask_noshear]['dec'], ra_units='deg', dec_units='deg', g1=g1, g2=g2, patch_centers=cat1.patch_centers)
+            
+                ng.process(cat1, cat2, initialize=(i==0), finalize=(i==len(cat2_files)-1))
                 cat2.unload()
             
             np.save('/global/cscratch1/sd/myamamot/metadetect/field_centers/cross_correlation_cov.npy', ng.cov)
@@ -440,8 +450,6 @@ def tangential_shear_field_center(fs):
             cat1_file = '/global/homes/m/myamamot/DES/des-y6-analysis/y6-combined-hsmap_random.fits'
             cat1 = treecorr.Catalog(cat1_file, ra_col='ra', dec_col='dec', ra_units='deg', dec_units='deg', npatch=20)
             cat2_files = glob.glob('/global/project/projectdirs/des/myamamot/metadetect/cuts_v2/*_metadetect-v5_mdetcat_part0000.fits')
-            # cat2_list = [treecorr.Catalog(cat2_file, ra_col='ra', dec_col='dec', ra_units='deg', dec_units='deg', g1_col='mdet_g_1', g2_col='mdet_g_2', patch_centers=cat1.patch_centers) for cat2_file in cat2_files]
-            cat2_list = []
             ng_rand = treecorr.NGCorrelation(bin_config, verbose=2)
             for i,cat2_f in enumerate(cat2_files):
                 d = fio.read(cat2_f)
