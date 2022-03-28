805f8d4e0ded3743158ceef42781fb5df79db5cc
Masaya Yamamoto <my137@duke.edu>
2022-03-28 11:42:36 -0400

    modify coadd meds

--- download-query-concatenation-code/modify_coadd_meds.py
+++ download-query-concatenation-code/modify_coadd_meds.py
@@ -9,7 +9,7 @@ from astropy.io import fits
 # f=glob.glob('/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/**/*.fits.fz', recursive=True)
 f=glob.glob('/global/cscratch1/sd/myamamot/pizza-slice/test/*.fits.fz', recursive=True)
 for coadd in f:
-    data1,hdr1 = fits.getdata(coadd, 'image_info' header=True)
-    data2,hdr2 = fits.getdata(coadd, 'epochs_info' header=True)
+    data1,hdr1 = fits.getdata(coadd, 'image_info', header=True)
+    data2,hdr2 = fits.getdata(coadd, 'epochs_info', header=True)
     fits.writeto(coadd, data1, header=hdr1)
     fits.append(coadd, data2, header=hdr2)
