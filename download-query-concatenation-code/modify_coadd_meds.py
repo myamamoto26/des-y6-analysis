3eaae896f6fce490036bbcadc6b9ecb01a76150f
Masaya Yamamoto <my137@duke.edu>
2022-03-28 12:03:18 -0400

    rsync test

--- download-query-concatenation-code/modify_coadd_meds.py
+++ download-query-concatenation-code/modify_coadd_meds.py
@@ -7,7 +7,9 @@ import glob
 from astropy.io import fits
 
 # f=glob.glob('/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/**/*.fits.fz', recursive=True)
-f=glob.glob('/global/cscratch1/sd/myamamot/pizza-slice/test/*.fits.fz', recursive=True)
+f = ['/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/r5763/DES0004-3540/p01/pizza-cutter/DES0004-3540_r5763p01_i_pizza-cutter-slices.fits.fz', 
+    '/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/r5763/DES0022-5914/p01/pizza-cutter/DES0022-5914_r5763p01_g_pizza-cutter-slices.fits.fz', 
+    '/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/r5763/DES0022-5914/p01/pizza-cutter/DES0022-5914_r5763p01_r_pizza-cutter-slices.fits.fz']
 for coadd in f:
     data1,hdr1 = fits.getdata(coadd, 'image_info', header=True)
     data2,hdr2 = fits.getdata(coadd, 'epochs_info', header=True)
