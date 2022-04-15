ed3d5cb1964de6b7616b35c682d8afaa125441d2
Masaya Yamamoto <my137@duke.edu>
2022-04-15 09:52:07 -0400

    rho JK cov path

--- catalog-validations-code/mdet_rho_stats.py
+++ catalog-validations-code/mdet_rho_stats.py
@@ -257,27 +257,27 @@ def main(argv):
     good_piffs_table = fio.read('/global/project/projectdirs/des/schutt20/catalogs/y6a2_piff_v2_hsm_allres_collated.fits')
 
     # rho-stats -> Just need to pass the piff catalog.
-    if not os.path.exists(os.path.join('/global/cscratch1/sd/myamamot/metadetect', 'rho_all_JK_griz.json')): 
+    if not os.path.exists(os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', 'rho_all_JK_griz.json')): 
         print('Computing rho-stats...')
         max_sep = 250
         max_mag = 0
         name = 'all_JK' 
         tag = 'griz'
         stats = measure_rho(good_piffs_table, max_sep, max_mag, subtract_mean=True, do_rho0=True)
-        stat_file = os.path.join('/global/cscratch1/sd/myamamot/metadetect', "rho_%s_%s.json"%(name, tag))
+        stat_file = os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', "rho_%s_%s.json"%(name, tag))
         write_stats(stat_file,*stats)
     else:
         print('Skipping rho-stats. ')
 
     # tau-stats. 
-    if not os.path.exists(os.path.join('/global/cscratch1/sd/myamamot/metadetect', 'tau_all_JK_griz.json')): 
+    if not os.path.exists(os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', 'tau_all_JK_griz.json')): 
         print('Computing tau-stats...')
         max_sep = 250
         max_mag = 0
         name = 'all_JK' 
         tag = 'griz'
         stats = measure_tau(good_piffs_table, max_sep, max_mag, subtract_mean=True)
-        stat_file = os.path.join('/global/cscratch1/sd/myamamot/metadetect', "tau_%s_%s.json"%(name, tag))
+        stat_file = os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', "tau_%s_%s.json"%(name, tag))
         write_stats_tau(stat_file,*stats)
     else:
         print('Skipping tau-stats. ')
