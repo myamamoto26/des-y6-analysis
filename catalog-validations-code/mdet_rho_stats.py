cced3826c5e05a5afede89c6ff3aacd99f6a376a
Masaya Yamamoto <my137@duke.edu>
2022-08-10 12:29:21 -0400

    ENH CLI-enabled, rho and tau-stats

--- catalog-validations-code/mdet_rho_stats.py
+++ catalog-validations-code/mdet_rho_stats.py
@@ -9,40 +9,67 @@ import pickle
 import glob
 from rho_stats import measure_rho, measure_tau, write_stats, write_stats_tau, plot_overall_rho, plot_overall_tao
 
-def main(argv):
+def measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, response_filepath, mdet_input_filepaths, stats_out_dir):
 
-    f = open('/global/project/projectdirs/des/myamamot/metadetect/mdet_files.txt', 'r')
-    fs = f.read().split('\n')[:-1]
-    mdet_filenames = [fname.split('/')[-1] for fname in fs]
-    tilenames = [d.split('_')[0] for d in mdet_filenames]
+    """
+    Computes rho-statistics from PIFF catalogs and tau-statistics from PIFF and metadetection catalogs.
+
+    Parameters
+    ----------
+    do_rho: whether or not do rho-stats (Boolean)
+    do_tau: whether or not do tau-stats(Boolean)
+    max_sep: maximum separation of the correlation function
+    Example) 60
+
+    stats_name:
+    Example) all_JK
+
+    response_filepath: 
+    Example) /global/cscratch1/sd/myamamot/metadetect/shear_response_v3.txt
+
+    mdet_input_filepaths: 
+    Example) /global/cscratch1/sd/myamamot/metadetect/cuts_v3/*_metadetect-v5_mdetcat_part0000.fits
+
+    stats_out_dir: 
+    Example) /global/cscratch1/sd/myamamot/metadetect/rho_tau_stats
+    """
+
     good_piffs_table = fio.read('/global/project/projectdirs/des/schutt20/catalogs/y6a2_piff_v2_hsm_allres_collated.fits')
 
     # rho-stats -> Just need to pass the piff catalog.
-    if False: #not os.path.exists(os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', 'rho_all_JK_v2_griz.json')): 
+    if do_rho: 
         print('Computing rho-stats...')
-        max_sep = 60
         max_mag = 0
-        name = 'all_JK_v2' 
+        name = stats_name 
         tag = 'griz'
         stats = measure_rho(good_piffs_table, max_sep, max_mag, subtract_mean=True, do_rho0=True)
-        stat_file = os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', "rho_%s_%s.json"%(name, tag))
+        stat_file = os.path.join(stats_out_dir, "rho_%s_%s.json"%(name, tag))
         write_stats(stat_file,*stats)
     else:
         print('Skipping rho-stats. ')
 
     # tau-stats. 
-    if not os.path.exists(os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', 'tau_all_JK_v3_griz.json')): 
+    if do_tau: 
         print('Computing tau-stats...')
-        max_sep = 60
         max_mag = 0
-        name = 'all_JK_v3' 
+        name = stats_name
         tag = 'griz'
-        stats = measure_tau(good_piffs_table, max_sep, max_mag, subtract_mean=True)
-        stat_file = os.path.join('/global/cscratch1/sd/myamamot/metadetect/rho_tau_stats', "tau_%s_%s.json"%(name, tag))
+        # Right now, it computes the tau-stats from the response corrected g1, g2, and the response is for over all the tiles. 
+        stats = measure_tau(good_piffs_table, max_sep, max_mag, response_filepath, mdet_input_filepaths, stats_out_dir, subtract_mean=True)
+        stat_file = os.path.join(stats_out_dir, "tau_%s_%s.json"%(name, tag))
         write_stats_tau(stat_file,*stats)
     else:
         print('Skipping tau-stats. ')
-    
+
+def main(argv):
+    do_rho = sys.argv[1]
+    do_tau = sys.argv[2]
+    max_sep = int(sys.argv[3])
+    stats_name = sys.argv[4]
+    response_filepath = sys.argv[5] 
+    mdet_input_filepaths = sys.argv[6]
+    stats_out_dir = sys.argv[7]
+    measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, response_filepath, mdet_input_filepaths, stats_out_dir)    
 
 if __name__ == "__main__":
     main(sys.argv)
