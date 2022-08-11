418ea8e7f226efaea2fe3dbe2f00431acd8e835d
Masaya Yamamoto <my137@duke.edu>
2022-08-11 16:31:14 -0400

    ENH use flat catalog

--- catalog-validations-code/mdet_rho_stats.py
+++ catalog-validations-code/mdet_rho_stats.py
@@ -4,12 +4,9 @@ import numpy as np
 import os, sys
 from tqdm import tqdm
 from matplotlib import pyplot as plt
-import emcee
-import pickle
-import glob
 from rho_stats import measure_rho, measure_tau, write_stats, write_stats_tau, plot_overall_rho, plot_overall_tao
 
-def measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, response_filepath, mdet_input_filepaths, stats_out_dir):
+def measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, mdet_input_flat, stats_out_dir):
 
     """
     Computes rho-statistics from PIFF catalogs and tau-statistics from PIFF and metadetection catalogs.
@@ -21,14 +18,11 @@ def measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, response_filepath
     max_sep: maximum separation of the correlation function
     Example) 60
 
-    stats_name:
+    stats_name: name of the statistics computed
     Example) all_JK
 
-    response_filepath: 
-    Example) /global/cscratch1/sd/myamamot/metadetect/shear_response_v3.txt
-
-    mdet_input_filepaths: 
-    Example) /global/cscratch1/sd/myamamot/metadetect/cuts_v3/*_metadetect-v5_mdetcat_part0000.fits
+    mdet_input_flat: the file path where the flat catalog is stored
+    Example) /global/cscratch1/sd/myamamot/sample_variance/data_catalogs_weighted_v3_snmax1000.pkl
 
     stats_out_dir: 
     Example) /global/cscratch1/sd/myamamot/metadetect/rho_tau_stats
@@ -54,8 +48,8 @@ def measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, response_filepath
         max_mag = 0
         name = stats_name
         tag = 'griz'
-        # Right now, it computes the tau-stats from the response corrected g1, g2, and the response is for over all the tiles. 
-        stats = measure_tau(good_piffs_table, max_sep, max_mag, response_filepath, mdet_input_filepaths, stats_out_dir, subtract_mean=True)
+        # Now it uses flat catalog created in make_flag_catalog.py
+        stats = measure_tau(good_piffs_table, max_sep, max_mag, mdet_input_flat, stats_out_dir, subtract_mean=True)
         stat_file = os.path.join(stats_out_dir, "tau_%s_%s.json"%(name, tag))
         write_stats_tau(stat_file,*stats)
     else:
@@ -66,10 +60,10 @@ def main(argv):
     do_tau = sys.argv[2]
     max_sep = int(sys.argv[3])
     stats_name = sys.argv[4]
-    response_filepath = sys.argv[5] 
-    mdet_input_filepaths = sys.argv[6]
-    stats_out_dir = sys.argv[7]
-    measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, response_filepath, mdet_input_filepaths, stats_out_dir)    
+    mdet_input_flat = sys.argv[5] 
+    stats_out_dir = sys.argv[6]
+
+    measure_rho_tau_stats(do_rho, do_tau, max_sep, stats_name, mdet_input_flat, stats_out_dir)    
 
 if __name__ == "__main__":
     main(sys.argv)
