c36a0736c9f46b2805c9c59863b586d6368163c1
Masaya Yamamoto <my137@duke.edu>
2022-09-09 15:41:16 -0400

    ENH accomodate for g-band only measurement

--- catalog-validations-code/cuts_and_save_catalogs.py
+++ catalog-validations-code/cuts_and_save_catalogs.py
@@ -6,6 +6,65 @@ from tqdm import tqdm
 import healsparse
 import numpy.lib.recfunctions as rf 
 
+def make_cuts_for_gband(argv):
+
+    mdet_files = sys.argv[1]
+    mdet_input_filepaths = sys.argv[2]
+    mdet_output_filepaths = sys.argv[3]
+    mask_map = sys.argv[4]
+    mdet_mom = sys.argv[5]
+    mdet_bands = sys.argv[6]
+
+    # Apply cuts + image masks, and save the catalogs.
+    hmap = healsparse.HealSparseMap.read(mask_map)
+    f = open(mdet_files, 'r')
+    fs = f.read().split('\n')[:-1]
+    mdet_filenames = [fname.split('/')[-1] for fname in fs]
+    tilenames = [d.split('_')[0] for d in mdet_filenames]
+
+    for fname in tqdm(mdet_filenames):
+
+        if os.path.exists(os.path.join(mdet_output_filepaths, fname)):
+            continue
+        elif not os.path.exists(os.path.join(mdet_input_filepaths, fname)):
+            print('no file found for this tile', fname)
+            continue
+        try:
+            d = fio.read(os.path.join(mdet_input_filepaths, fname))
+        except:
+            print(fname)
+            continue
+
+        mag_g = 30.0 - 2.5*np.log10(d[mdet_mom+"_band_flux_g"])
+        msk = ((d[mdet_mom+"_flags"] == 0) & 
+            (d["mask_flags"] == 0) & 
+            (d["shear_bands"] == mdet_bands) & 
+            (d[mdet_mom+"_band_flux_flags_g"] == 0) & 
+            (d[mdet_mom+"_s2n"] > 10) & 
+            (d["mfrac"] < 0.1) &
+            np.isfinite(mag_g) & 
+            (mag_g < 26.5))
+
+        if mdet_mom == 'wmom':
+            msk_add = ((d[mdet_mom+"_T_ratio"] > 1.2))
+            msk = msk & msk_add
+        elif mdet_mom == 'pgauss':
+            msk_add = ((d[mdet_mom+"_T_ratio"] > 0.5) & (d[mdet_mom+"_T"] < 1.9 - 2.8*d[mdet_mom+"_T_err"]))
+            msk = msk & msk_add
+        elif mdet_mom == 'pgauss_reg0.90':
+            msk_add = ((d[mdet_mom+"_T_ratio"] > 0.5) & (d[mdet_mom+"_T"] < 1.9 - 2.8*d[mdet_mom+"_T_err"]))
+            msk = msk & msk_add
+        
+        in_footprint = hmap.get_values_pos(d["ra"], d["dec"], valid_mask=True)
+
+        total_msk = (msk & in_footprint)
+        d_msk = d[total_msk]
+
+        if mdet_mom == 'pgauss':
+            columns = ['slice_id', 'mdet_step', 'ra', 'dec', 'ra_noshear', 'dec_noshear', 'y_noshear', 'x_noshear', 'y', 'x', 'slice_y', 'slice_x', 'slice_y_noshear', 'slice_x_noshear', 'hpix_16384', 'hpix_16384_noshear', 'filename', 'tilename', 'mask_flags', 'mask_flags_noshear', 'nepoch_g','nepoch_eff_g', 'pgauss_flags', 'pgauss_psf_flags', 'pgauss_psf_g_1', 'pgauss_psf_g_2', 'pgauss_psf_T', 'pgauss_obj_flags', 'pgauss_s2n', 'pgauss_g_1', 'pgauss_g_2', 'pgauss_g_cov_1_1', 'pgauss_g_cov_1_2', 'pgauss_g_cov_2_2', 'pgauss_T', 'pgauss_T_flags', 'pgauss_T_err', 'pgauss_T_ratio', 'pgauss_band_flux_flags_g', 'pgauss_band_flux_g', 'pgauss_band_flux_err_g', 'shear_bands', 'ormask', 'mfrac', 'bmask', 'mfrac_img', 'ormask_noshear', 'mfrac_noshear', 'bmask_noshear', 'psfrec_flags', 'psfrec_g_1', 'psfrec_g_2', 'psfrec_T']
+
+        d_out = rf.repack_fields(d_msk[columns])
+        fio.write(os.path.join(mdet_output_filepaths, fname), d_out)
 
 def make_cuts_and_save_catalogs(argv):
     """Saves metadetection catalogs in /CSCRATCH space with appropriate selection cuts from raw metadetection catalogs.
@@ -57,7 +116,8 @@ def make_cuts_and_save_catalogs(argv):
         except:
             print(fname)
             continue
-
+            
+        
         mag_g = 30.0 - 2.5*np.log10(d[mdet_mom+"_band_flux_g"])
         mag_r = 30.0 - 2.5*np.log10(d[mdet_mom+"_band_flux_r"])
         mag_i = 30.0 - 2.5*np.log10(d[mdet_mom+"_band_flux_i"])
@@ -113,4 +173,8 @@ def make_cuts_and_save_catalogs(argv):
         fio.write(os.path.join(mdet_output_filepaths, fname), d_out)
 
 if __name__ == "__main__":
-    make_cuts_and_save_catalogs(sys.argv)
+
+    if sys.argv[6] == '0':
+        make_cuts_for_gband(sys.argv)
+    else:
+        make_cuts_and_save_catalogs(sys.argv)
