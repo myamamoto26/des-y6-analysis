a00e69e1d9cdf1ee81b29561733faef7c732a6a5
Masaya Yamamoto <my137@duke.edu>
2022-09-12 11:19:42 -0400

    ENH save stats for g-band only measurement

--- catalog-validations-code/save_stats.py
+++ catalog-validations-code/save_stats.py
@@ -4,6 +4,46 @@ import glob
 import os, sys
 from tqdm import tqdm
 
+def _save_stats_g(mdet_files, mdet_mom, outpath, stats_file, add_cuts=None):
+
+    res = np.zeros(200000000, dtype=[('ra', float), ('psfrec_g_1', float), ('psfrec_g_2', float), ('psfrec_T', float), (mdet_mom+'_s2n', float), (mdet_mom+'_T', float), (mdet_mom+'_T_ratio', float), (mdet_mom+'_g_1', float), (mdet_mom+'_g_2', float), ('dec', float), (mdet_mom+'_g_cov_1_1', float), (mdet_mom+'_g_cov_2_2', float), (mdet_mom+'_T_err', float), (mdet_mom+"_band_flux_g", float), ('nepoch_eff_g', int), ('nepoch_g', int)])
+
+    start = 0
+    for f in tqdm(mdet_files):
+        d = fio.read(f)
+        d = d[d['mdet_step'] == 'noshear']
+        if add_cuts:
+            for cut in add_cuts:
+                if cut == mdet_mom+'_s2n':
+                    d = d[d[cut] < 200]
+                elif cut == mdet_mom+'_T_ratio':
+                    d = d[d[cut] > 1.5]
+        end = start+len(d)
+
+        res['ra'][start:end] = d['ra']
+        res['psfrec_g_1'][start:end] = d['psfrec_g_1']
+        res['psfrec_g_2'][start:end] = d['psfrec_g_2']
+        res['psfrec_T'][start:end] = d['psfrec_T']
+        res[mdet_mom+'_s2n'][start:end] = d[mdet_mom+'_s2n']
+        res[mdet_mom+'_T'][start:end] = d[mdet_mom+'_T']
+        res[mdet_mom+'_T_ratio'][start:end] = d[mdet_mom+'_T_ratio']
+        res[mdet_mom+'_g_1'][start:end] = d[mdet_mom+'_g_1']
+        res[mdet_mom+'_g_2'][start:end] = d[mdet_mom+'_g_2']
+        res['dec'][start:end] = d['dec']
+        res[mdet_mom+'_g_cov_1_1'][start:end] = d[mdet_mom+'_g_cov_1_1']
+        res[mdet_mom+'_g_cov_2_2'][start:end] = d[mdet_mom+'_g_cov_2_2']
+        res[mdet_mom+'_T_err'][start:end] = d[mdet_mom+'_T_err']
+        res[mdet_mom+'_band_flux_g'][start:end] = d[mdet_mom+'_band_flux_g']
+        res['nepoch_eff_g'][start:end] = d['nepoch_eff_g']
+        res['nepoch_g'][start:end] = d['nepoch_g']
+
+        start = end
+
+    # remove zero entry
+    res = res[res['ra'] != 0]
+    print('number of objects ', len(res))
+    fio.write(os.path.join(outpath, stats_file), res)
+
 def _save_stats(mdet_files, mdet_mom, outpath, stats_file, add_cuts=None):
 
     res = np.zeros(200000000, dtype=[('ra', float), ('psfrec_g_1', float), ('psfrec_g_2', float), ('psfrec_T', float), (mdet_mom+'_s2n', float), (mdet_mom+'_T', float), (mdet_mom+'_T_ratio', float), (mdet_mom+'_g_1', float), (mdet_mom+'_g_2', float), ('dec', float), (mdet_mom+'_g_cov_1_1', float), (mdet_mom+'_g_cov_2_2', float), ('g-r', float), ('r-i', float), ('i-z', float), (mdet_mom+'_T_err', float), (mdet_mom+"_band_flux_g", float), (mdet_mom+"_band_flux_r", float), (mdet_mom+"_band_flux_i", float), (mdet_mom+"_band_flux_z", float), ('nepoch_eff_g', int), ('nepoch_g', int)])
@@ -75,7 +115,10 @@ def main(argv):
             mdet_files.append(fn)
     print('there are ', len(mdet_files), ' to be processed.')
 
-    _save_stats(mdet_files, mdet_mom, outpath, stats_file)
+    if shear_bands == 'g':
+        _save_stats_g(mdet_files, mdet_mom, outpath, stats_file)
+    else:
+        _save_stats(mdet_files, mdet_mom, outpath, stats_file)
 
 if __name__ == "__main__":
     main(sys.argv)
