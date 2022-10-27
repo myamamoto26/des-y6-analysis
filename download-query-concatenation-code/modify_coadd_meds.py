50c34866c84f65c59cf2821ac9eb11646cf9b265
myamamoto26 <masaya.yamamoto@duke.edu>
2022-10-27 08:36:36 -0700

    ENH download pizza-cutter meds files from desdm and cut down info

--- download-query-concatenation-code/modify_coadd_meds.py
+++ download-query-concatenation-code/modify_coadd_meds.py
@@ -6,16 +6,32 @@ import numpy as np
 import glob
 from astropy.io import fits
 from tqdm import tqdm
+import subprocess
 
-f=glob.glob('/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/**/*.fits.fz', recursive=True)
-# f = ['/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/r5763/DES0004-3540/p01/pizza-cutter/DES0004-3540_r5763p01_i_pizza-cutter-slices.fits.fz', 
-#     '/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/r5763/DES0022-5914/p01/pizza-cutter/DES0022-5914_r5763p01_g_pizza-cutter-slices.fits.fz', 
-#     '/global/project/projectdirs/des/myamamot/pizza-slice/OPS/multiepoch/Y6A2_PIZZACUTTER/r5763/DES0022-5914/p01/pizza-cutter/DES0022-5914_r5763p01_r_pizza-cutter-slices.fits.fz']
-for coadd in tqdm(f):
-    if len(fits.open(coadd)) == 3:
-        continue
-    print('rewriting', coadd)
-    data1,hdr1 = fits.getdata(coadd, 'image_info', header=True)
-    data2,hdr2 = fits.getdata(coadd, 'epochs_info', header=True)
-    fits.writeto(coadd, data1, header=hdr1, overwrite=True)
-    fits.append(coadd, data2, header=hdr2)
+pizza_info = fio.read('/global/cfs/cdirs/des/myamamot/pizza-slice/pizza-cutter-coadds-info.fits')
+band = ['z']
+no_files = ["DES2357-5040_r5992p01_r_pizza-cutter-slices.fits", "DES0410-5831_r5934p01_r_pizza-cutter-slices.fits", "DES0105+0126_r5932p01_r_pizza-cutter-slices.fits", "DES0542-3832_r5982p01_r_pizza-cutter-slices.fits", "DES0236-5248_r5933p01_i_pizza-cutter-slices.fits", "DES2127-5123_r5992p01_i_pizza-cutter-slices.fits", "DES2302-6414_r5992p01_z_pizza-cutter-slices.fits"]
+for path,fname in tqdm(zip(pizza_info['PATH'], pizza_info['FILENAME'])):
+    filepath = os.path.join(path, fname+'.fz')
+
+    if fname.split('_')[2] in band:
+        cmd = """rsync -av --ignore-existing --password-file /global/cfs/cdirs/des/myamamot/pizza-slice/rsync_pass.txt masaya.yamamoto@desar2.cosmology.illinois.edu::ALLDESFiles/new_archive/desarchive/%s /global/cfs/cdirs/des/myamamot/pizza-slice/data/%s""" % (filepath, fname+'.fz')
+
+        coadd = os.path.join('/global/cfs/cdirs/des/myamamot/pizza-slice/data', fname+'.fz')
+        if fname in no_files :
+            print(filepath)
+            continue
+        if not os.path.exists(coadd):
+            try:
+                subprocess.run(cmd, shell=True, check=True)
+            except:
+                print(filepath)
+
+        if len(fits.open(coadd)) == 3:
+            print('cannot rewrite', coadd)
+            continue
+
+        data1,hdr1 = fits.getdata(coadd, 'image_info', header=True)
+        data2,hdr2 = fits.getdata(coadd, 'epochs_info', header=True)
+        fits.writeto(coadd, data1, header=hdr1, overwrite=True)
+        fits.append(coadd, data2, header=hdr2)
